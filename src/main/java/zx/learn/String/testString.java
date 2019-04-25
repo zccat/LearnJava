2949a0a6867687b0d5b14d78bf57923c72726e9d
紫川 <xox.moe@outlook.com>
2019-04-25 11:18:20 +0800

    提交一下

---
+++ src/main/java/zx/learn/String/testString.java
@@ -0,0 +1,16 @@
+package zx.learn.String;
+
+/**
+ * @Auther: 胡志新
+ * @Date: 2019/4/25 11:10
+ * @Description:
+ */
+public class testString {
+
+    public static void main(String[] args) {
+        String test = " ";
+        Boolean b = (test == null || "".equals(test));
+        System.out.print(b);
+    }
+
+}
